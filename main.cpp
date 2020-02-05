f12becb5257f070ad40a11d40b51a90906971632
Krzysztof_Majda <krzyyyy@gmail.com>
2020-02-05 12:03:59 +0100

    Zmiana trybów zasilania

--- main.cpp
+++ main.cpp
@@ -3,13 +3,22 @@
 #include <vector>
 #include "dataTypes.h"
 #include <cstring>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 void loop() {
+	auto start = std::chrono::system_clock::now();
+	// Some computation here
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
 	double i = 0;
-	while (true) {
+	do {
 		i = i+1.;
-	}
+		end = std::chrono::system_clock::now();
+		elapsed_seconds = end - start;
+		//std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+	} while (elapsed_seconds.count()<20.);
 }
 
 
@@ -32,27 +41,17 @@ void printPowerInfo(const SYSTEM_POWER_POLICY& powerInfo) {
 }
 
 int main() {
-	POWER_INFORMATION_LEVEL InformationLevel = SystemPowerInformation;
-    SYSTEM_POWER_POLICY powerInfo = SYSTEM_POWER_POLICY();
-	PVOID OutputBuffer = PVOID();
-	ULONG OutputBufferLength;
-	POWER_DATA_ACCESSOR AccessFlags = POWER_DATA_ACCESSOR();
-	UINT pwrSchame = UINT();
-	PUINT number  = &pwrSchame;
-	GUID g;
-	LPWSTR guidstr = LPWSTR("{8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c}");
-	CLSID ff;
-	//wchar_t* clsid_str = L"{5C98B8E4-2B3D-12B1-ADE2-0000F86456B23}";
-	/*CLSIDFromString(guidstr, &ff);
-	auto ff = GetActivePwrScheme(number);*/
-	//DWORD flag = PowerSettingAccessCheck( AccessFlags, );
- 	NTSTATUS status = CallNtPowerInformation(SystemPowerPolicyDc, NULL, NULL, &powerInfo, sizeof(powerInfo));
-	printPowerInfo(powerInfo);
+	GUID *schemat = nullptr;
+	auto ok_get = PowerGetActiveScheme(0, &schemat);
+
+	auto ok_set = PowerSetActiveScheme(0, &GUID_MIN_POWER_SAVINGS);
+	
 	auto threads2 = vector <thread>(0);
 	for(int i=0;i<4;++i)
 		threads2.push_back(thread(loop));
 
 	for (auto& th : threads2)
 		th.join();
+	auto ok_get2 = PowerSetActiveScheme(0, schemat);
 	return 0;
 }
