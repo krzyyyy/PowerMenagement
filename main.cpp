1d9766ce7f7f8134f4537709c4021f6529fd604d
Krzysztof_Majda <krzyyyy@gmail.com>
2020-02-18 12:12:53 +0100

    ostateczna wersja

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <cstring>
 #include <chrono>
 #include <ctime>
+#include <Windows.h>
+#include <powrprof.h>
+//#include <powerbase.h>
 
 using namespace std;
 void loop() {
@@ -22,30 +25,12 @@ void loop() {
 }
 
 
-void printPowerInfo(const SYSTEM_POWER_POLICY& powerInfo) {
-	auto boolToStr = [](const bool& var) {
-		if (var == true)
-			return "TRUE";
-		return "FALSE";
-	};
-	cout << "Revision  " << powerInfo.Revision << endl;
-	cout << "FanThrottleTolerance  " << powerInfo.FanThrottleTolerance << endl;
-	cout << "ForcedThrottle  " << powerInfo.ForcedThrottle << endl;
-	cout << "MinThrottle  " << int(powerInfo.MinThrottle) << endl;
-	cout << "OptimizeForPower  " << boolToStr(powerInfo.OptimizeForPower) << endl;
-	cout << "OverThrottled.Action  " << powerInfo.OverThrottled.Action << endl;
-	cout << "OverThrottled.EventCode  " << powerInfo.OverThrottled.EventCode << endl;
-	cout << "OverThrottled.Flags  " << powerInfo.OverThrottled.Flags << endl;
-	cout << "DynamicThrottle  " << int(powerInfo.DynamicThrottle) << endl;
-
-}
 
 int main() {
 	GUID *schemat = nullptr;
 	auto ok_get = PowerGetActiveScheme(0, &schemat);
-
 	auto ok_set = PowerSetActiveScheme(0, &GUID_MIN_POWER_SAVINGS);
-	
+
 	auto threads2 = vector <thread>(0);
 	for(int i=0;i<4;++i)
 		threads2.push_back(thread(loop));
